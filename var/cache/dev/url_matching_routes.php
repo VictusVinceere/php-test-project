d92d45935158b5780dd4edc67020a960dba1541e
Vvicii <vinceere@gmail.com>
2020-09-24 21:53:23 +0500

    updating environment, adding seperate classes

---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/client/save' => [[['_route' => 'client', '_controller' => 'App\\Controller\\ClientController::saveData'], null, null, null, false, false, null]],
+        '/purchase/save' => [[['_route' => 'purchase', '_controller' => 'App\\Controller\\PurchaseController::saveData'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
